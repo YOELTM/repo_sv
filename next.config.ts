8db8d34227d3f8768afdf0783aae6b50c659a050
yoex <ytentertainemnt94@gmail.com>
2025-02-09 17:21:15 -0400

    segundo deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true, // Necesario si usas imágenes de Next.js
-  },
 };
 
 module.exports = nextConfig;
